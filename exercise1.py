89d679d4a5a3fabc38a470206134c866e524b062
pancan <verifx@gmx.de>
2024-10-07 16:28:07 +0200

    Create a copy to fix assertion error

--- exercise1.py
+++ exercise1.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 from typing import List
 from math import isclose
 
-# Task A: see comment in function `test_vector_index_access`
 # Task B: see comment in function `test_3d_vector_subtraction`
 
 class Vector:
     def __init__(self, coordinates: List[float]) -> None:
-        # self._coordinates = coordinates.copy()
-        self._coordinates = coordinates # Task A
+        self._coordinates = coordinates
 
     def __getitem__(self, i: int) -> float:
         return self._coordinates[i]
@@ -33,7 +31,6 @@ def test_vector_index_access() -> None:
         vector[index] = 42.0
         assert vector[index] == 42.0
 
-        # Task A: make this test pass by ensuring that `Vector` uses a copy of the coordinates it receives in the constructor
         assert reference[index] != 42.0
 
 
