3265d07d5c7c604888e1696588120df6d2fcb6a3
Lukas Piller <61459694+iTaykon@users.noreply.github.com>
2024-10-07 16:43:13 +0200

    Added Vector test for Subtraction (#1)
    Co-authored-by: Lukas Piller <lukas.piller@ipvs.uni-stuttgart.de>
    Co-authored-by: pancan21 <75097835+pancan21@users.noreply.github.com>

--- exercise1.py
+++ exercise1.py
@@ -42,10 +42,12 @@ def test_3d_vector_addition() -> None:
 
 
 def test_3d_vector_subtraction() -> None:
-    # Task B: add a test for vector subtraction
-    assert True
+    v = Vector([2.0, 5.5, 6.3]) - Vector([1.2, 4.0, 2.0])
+    assert isclose(v[0], 0.8)
+    assert isclose(v[1], 1.5)
+    assert isclose(v[2], 4.3)
 
-if __name__ == "__main__":
+if __name__ == "__main__":.5
     test_vector_index_access()
     test_3d_vector_addition()
     test_3d_vector_subtraction()
